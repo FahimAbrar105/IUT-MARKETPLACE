748c1d3815897d6671c2a4908777d32532e9827b
Farhan Shahriyar <farhanhossain@iut-dhaka.edu>
2026-02-07 15:45:00 +0600

    added product routes and image upload configuration

---
+++ routes/products.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getProducts,
+    getProduct,
+    createProductForm,
+    createProduct,
+    deleteProduct
+} = require('../controllers/productController');
+const { protect } = require('../middleware/auth');
+const upload = require('../middleware/upload');
+
+router.get('/', getProducts);
+
+router.get('/create', protect, createProductForm);
+
+router.post('/', protect, (req, res, next) => {
+    upload.array('images', 5)(req, res, (err) => {
+        if (err) {
+            console.error('[UPLOAD ERROR]', err);
+            return res.render('products/create', {
+                title: 'Sell Item',
+                user: req.user,
+                error: 'Image Upload Failed: ' + err.message,
+                formData: req.body
+            });
+        }
+        next();
+    });
+}, createProduct);
+
+router.get('/:id', getProduct);
+
+router.post('/:id/delete', protect, deleteProduct);
+
+module.exports = router;
